ccfb565c110dd19201e83837bd77cb85a26e3c52
pluns <peter.luns@outlook.com>
2026-03-27 11:12:49 +0100

    Initial Commit

---
+++ FocusFixr/FocusFixrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFixrApp.swift
+//  FocusFixr
+//
+//  Created by Adriana Luns on 27/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusFixrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
