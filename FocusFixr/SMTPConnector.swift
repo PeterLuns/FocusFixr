ae283ca660edeeac97026485566448231485b764
Peter Luns <luns@xs4all.nl>
2026-03-31 22:52:33 +0200

    project cleanup

--- FocusFixr/SMTPConnector.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SMTPConnector.swift
-//  FocusFixr
-//
-//  Created by Adriana Luns on 28/03/2026.
-//
-
-import Foundation
--- FocusFixrTests/FocusFixrTests.swift
+++ FocusFixrTests/FocusFixrTests.swift
@@ -10,8 +10,48 @@ import Testing
 
 struct FocusFixrTests {
 
-    @Test func example() async throws {
+    @MainActor @Test func initializeMailConnector()  {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let mailConnector: MailConnector = MailConnector()
+        
+        #expect(mailConnector.smtpHost == "mail.mijndomein.nl" )
+        #expect(mailConnector.smtpPort == "587" )
+        #expect(mailConnector.smtpUser == "info@adrianaluns.nl" )
+
+//        #expect(mailConnector.imapHost == "imap.xs4all.nl" )
+//        #expect(mailConnector.imapPort == "993" )
+//        #expect(mailConnector.imapUser == "luns@xs4all.nl" )
+
+    }
+    
+    @MainActor @Test func setSMTPParams()  {
+        let mailConnector: MailConnector = MailConnector()
+        
+        mailConnector.setSMTPParams(smtpHost: "test.com", smtpPort: 465, smtpUser: "testuser", smtpPWD: "testpwd")
+        
+        #expect(mailConnector.smtpHost == "test.com" )
+        #expect(mailConnector.smtpPort == "465" )
+        #expect(mailConnector.smtpUser == "testuser" )
+    }
+    
+    @Test func connectSMPT() async {
+        let mailConnector: MailConnector = await MailConnector()
+        
+        await mailConnector.connectSMTP()
+
+        #expect(mailConnector.isSMTPConnected == true)
+    }
+    
+    @Test func testSendMail() async throws {
+
+        let mailConnector: MailConnector = await MailConnector()
+        
+        await mailConnector.connectSMTP()
+        
+        let result = await mailConnector.sendSMTPEmail(to: "peter.luns@outlook.com", recipients: [], subject: "Test", body: "This is a test email.")
+        
+        #expect(result == true)
+
     }
 
 }
